925424a928c0217579fd9a90a8140ce07c31c112
Mario Gutierrez <mag262@txstate.edu>
2015-08-26 13:08:35 -0500

    Data analysis section

--- bac-prediction/scripts/functions.r
+++ bac-prediction/scripts/functions.r
@@ -20,7 +20,7 @@ rescale_01 <- function(x) {
 #	 use_linear - fit a line to the 'front' data if TRUE, fits a curve otherwise
 # 		(default: TRUE).
 # Returns: the resulting ggplot2 object.
-fadoodle <- function(data, bac, front, front.label="", use_linear=TRUE) {
+fadoodle <- function(data, bac, front, front.label="", curve='l') {
 	require('ggplot2')
 	
 	ifelse(front.label == "", front.label <- toupper(front), front.label <- toupper(front.label))
@@ -31,15 +31,48 @@ fadoodle <- function(data, bac, front, front.label="", use_linear=TRUE) {
 		geom_point(aes_string(x = "timestamp", y = front), color = "red", size = 1.5) +
 		theme(axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank()) +
 		ylab(ylab) +
-		xlab("TIME")
-		
-	if (use_linear == FALSE) {
+		xlab("")
+	
+	if (!is.null(curve))	{
+		if (curve == 's') {
 			result <- result + geom_smooth(aes_string(x = "timestamp", y = front))
-	} else {
-		result <- result + geom_smooth(aes_string(x = "timestamp", y = front), method="lm")
+		} else {
+			result <- result + geom_smooth(aes_string(x = "timestamp", y = front), method="lm")
+		}
 	}
+	
+	result <- result + facet_wrap( ~ subject, ncol=3)
 	
 	return(result)
 }
+
+# Draws a scatterplot with a curve over it, and a line plot in the background.
+# Author: Mario A. Gutierrez (mag262@txstate.edu)
+# Date: 08/23/2015   Version: 1.02
+#	 data - is the data.frame with the data
+#	 back - the data for the line plot in the back.
+#	 front - the data for the scatterplot in the front.
+#	 use_linear - fit a line to the 'front' data if TRUE, fits a curve otherwise
+# 		(default: TRUE).
+# Returns: the resulting ggplot2 object.
+splithr <- function(data, bac, front, split_bpm=85, front.label="") {
+	require('ggplot2')
+	
+	ifelse(front.label == "", front.label <- toupper(front), front.label <- toupper(front.label))
+	ylab <- paste("BAC vs.", front.label)
+	
+	colors <- rep("red", nrow(data))
+	colors[data$ms_heart_rate_bpm < split_bpm] <- "blue"
+	
+	result <- ggplot(data, aes_string(x = "timestamp", y = bac)) +
+		geom_line() +
+		geom_point(aes_string(x = "timestamp", y = front), color = colors, size = 1.5) +
+		theme(axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank()) +
+		ylab(ylab) +
+		xlab("")
+	
+	return(result)
+}
+
 
 
